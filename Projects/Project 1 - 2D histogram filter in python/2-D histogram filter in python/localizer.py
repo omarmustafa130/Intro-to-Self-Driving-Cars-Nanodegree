650fefd0aec7c2da314eacd830a98f6ee46f3593
Omar Mustafa <omarmustafa130@gmail.com>
2023-01-16 23:22:17 +0200

    Update localizer.py

--- Projects/Project 1 - 2D histogram filter in python/2-D histogram filter in python/localizer.py
+++ Projects/Project 1 - 2D histogram filter in python/2-D histogram filter in python/localizer.py
@@ -39,10 +39,10 @@ def move(dy, dx, beliefs, blurring):
     height = len(beliefs)
     width = len(beliefs[0])
     new_G = [[0.0 for i in range(width)] for j in range(height)]
-    for r, row in enumerate(beliefs):
-        for k, cell in enumerate(row):
-            new_i = (r + dy ) % height
-            new_j = (k + dx ) % width
+    for i, row in enumerate(beliefs):
+        for j, cell in enumerate(row):
+            new_i = (i + dy) % height
+            new_j = (j + dx) % width
             #pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
     return blur(new_G, blurring)
